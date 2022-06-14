dd85c0ac401487705fb4ac38d7a811af923786bf
CristobalRodriguezCh <cantoniorodriguez2307@gmail.com>
2022-06-14 01:06:49 -0500

    primer comit

---
+++ app/Models/User.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class User extends Authenticatable implements JWTSubject
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     * 
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    //se obtiene el identificador que se almacenara
+    //en una parte del jwt
+    public function getJWTIdentifier(){
+        return $this->getKey();
+    }
+
+    /**
+        * devuelve un arreglo de reclamos personalizados que se le agregara al JWt
+        * @return array
+    */
+    public function getJWTCustomClaims() {
+        return [];
+    }
+}
